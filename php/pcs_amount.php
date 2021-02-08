ba77f33fb33e13a48b1271e0419d30c817fdff7c
kenshin <dx0w0xb@lookout.com>
2021-02-08 10:33:54 +0800

    coudan

---
+++ php/pcs_amount.php
@@ -0,0 +1,237 @@
+<?php
+
+
+$orders = [
+    ['id' => 1, 'amount' => 100020],
+    ['id' => 2, 'amount' => 200030],
+];
+
+$items = [
+    ['id' => 'i1', 'pcs' => 10, 'per_price' => 1000, 'amount' => 10000],
+    ['id' => 'i2', 'pcs' => 9, 'per_price' => 1200, 'amount' => 10800],
+    ['id' => 'i3', 'pcs' => 6, 'per_price' => 800, 'amount' => 4800],
+    ['id' => 'i4', 'pcs' => 3, 'per_price' => 600, 'amount' => 1800],
+    ['id' => 'i5', 'pcs' => 20, 'per_price' => 400, 'amount' => 8000],
+    ['id' => 'i6', 'pcs' => 13, 'per_price' => 556, 'amount' => 7228],
+    ['id' => 'i7', 'pcs' => 1, 'per_price' => 1000, 'amount' => 1000],
+    ['id' => 'ii1', 'pcs' => 10, 'per_price' => 1000, 'amount' => 10000],
+    ['id' => 'ii2', 'pcs' => 9, 'per_price' => 1200, 'amount' => 10800],
+    ['id' => 'ii3', 'pcs' => 6, 'per_price' => 800, 'amount' => 4800],
+    ['id' => 'ii4', 'pcs' => 3, 'per_price' => 600, 'amount' => 1800],
+    ['id' => 'ii5', 'pcs' => 20, 'per_price' => 400, 'amount' => 8000],
+    ['id' => 'ii6', 'pcs' => 13, 'per_price' => 556, 'amount' => 7228],
+    ['id' => 'ii7', 'pcs' => 1, 'per_price' => 1000, 'amount' => 1000],
+    ['id' => 'iii1', 'pcs' => 10, 'per_price' => 1000, 'amount' => 10000],
+    ['id' => 'iii2', 'pcs' => 9, 'per_price' => 1200, 'amount' => 10800],
+    ['id' => 'iii3', 'pcs' => 6, 'per_price' => 800, 'amount' => 4800],
+    ['id' => 'iii4', 'pcs' => 3, 'per_price' => 600, 'amount' => 1800],
+    ['id' => 'iii5', 'pcs' => 20, 'per_price' => 400, 'amount' => 8000],
+    ['id' => 'iii6', 'pcs' => 13, 'per_price' => 556, 'amount' => 7228],
+    ['id' => 'iii7', 'pcs' => 1, 'per_price' => 1000, 'amount' => 1000],
+    ['id' => 'iiii1', 'pcs' => 10, 'per_price' => 1000, 'amount' => 10000],
+    ['id' => 'iiii2', 'pcs' => 9, 'per_price' => 1200, 'amount' => 10800],
+    ['id' => 'iiii3', 'pcs' => 6, 'per_price' => 800, 'amount' => 4800],
+    ['id' => 'iiii4', 'pcs' => 3, 'per_price' => 600, 'amount' => 1800],
+    ['id' => 'iiii5', 'pcs' => 20, 'per_price' => 400, 'amount' => 8000],
+    ['id' => 'iiii6', 'pcs' => 13, 'per_price' => 556, 'amount' => 7228],
+    ['id' => 'iiii7', 'pcs' => 1, 'per_price' => 1000, 'amount' => 1000],
+    ['id' => 'iiiii1', 'pcs' => 10, 'per_price' => 1000, 'amount' => 10000],
+    ['id' => 'iiiii2', 'pcs' => 9, 'per_price' => 1200, 'amount' => 10800],
+    ['id' => 'iiiii3', 'pcs' => 6, 'per_price' => 800, 'amount' => 4800],
+    ['id' => 'iiiii4', 'pcs' => 3, 'per_price' => 600, 'amount' => 1800],
+    ['id' => 'iiiii5', 'pcs' => 20, 'per_price' => 400, 'amount' => 8000],
+    ['id' => 'iiiii6', 'pcs' => 13, 'per_price' => 556, 'amount' => 7228],
+    ['id' => 'iiiii7', 'pcs' => 1, 'per_price' => 1000, 'amount' => 1000],
+    ['id' => 'iiiiii1', 'pcs' => 10, 'per_price' => 1000, 'amount' => 10000],
+    ['id' => 'iiiiii2', 'pcs' => 9, 'per_price' => 1200, 'amount' => 10800],
+    ['id' => 'iiiiii3', 'pcs' => 6, 'per_price' => 800, 'amount' => 4800],
+    ['id' => 'iiiiii4', 'pcs' => 3, 'per_price' => 600, 'amount' => 1800],
+    ['id' => 'iiiiii5', 'pcs' => 20, 'per_price' => 400, 'amount' => 8000],
+    ['id' => 'iiiiii6', 'pcs' => 13, 'per_price' => 556, 'amount' => 7228],
+    ['id' => 'iiiiii7', 'pcs' => 1, 'per_price' => 1000, 'amount' => 1000],
+    ['id' => 'iiiiiii1', 'pcs' => 10, 'per_price' => 1000, 'amount' => 10000],
+    ['id' => 'iiiiiii2', 'pcs' => 9, 'per_price' => 1200, 'amount' => 10800],
+    ['id' => 'iiiiiii3', 'pcs' => 6, 'per_price' => 800, 'amount' => 4800],
+    ['id' => 'iiiiiii4', 'pcs' => 3, 'per_price' => 600, 'amount' => 1800],
+    ['id' => 'iiiiiii5', 'pcs' => 20, 'per_price' => 400, 'amount' => 8000],
+    ['id' => 'iiiiiii6', 'pcs' => 13, 'per_price' => 556, 'amount' => 7228],
+    ['id' => 'iiiiiii7', 'pcs' => 1, 'per_price' => 1000, 'amount' => 1000],
+    ['id' => 'iiiiiiii1', 'pcs' => 10, 'per_price' => 1000, 'amount' => 10000],
+    ['id' => 'iiiiiiii2', 'pcs' => 9, 'per_price' => 1200, 'amount' => 10800],
+    ['id' => 'iiiiiiii3', 'pcs' => 6, 'per_price' => 800, 'amount' => 4800],
+    ['id' => 'iiiiiiii4', 'pcs' => 3, 'per_price' => 600, 'amount' => 1800],
+    ['id' => 'iiiiiiii5', 'pcs' => 20, 'per_price' => 400, 'amount' => 8000],
+    ['id' => 'iiiiiiii6', 'pcs' => 13, 'per_price' => 556, 'amount' => 7228],
+    ['id' => 'iiiiiiii7', 'pcs' => 1, 'per_price' => 1000, 'amount' => 1000],
+    ['id' => 'iiiiiiiii1', 'pcs' => 10, 'per_price' => 1000, 'amount' => 10000],
+    ['id' => 'iiiiiiiii2', 'pcs' => 9, 'per_price' => 1200, 'amount' => 10800],
+    ['id' => 'iiiiiiiii3', 'pcs' => 6, 'per_price' => 800, 'amount' => 4800],
+    ['id' => 'iiiiiiiii4', 'pcs' => 3, 'per_price' => 600, 'amount' => 1800],
+    ['id' => 'iiiiiiiii5', 'pcs' => 20, 'per_price' => 400, 'amount' => 8000],
+    ['id' => 'iiiiiiiii6', 'pcs' => 13, 'per_price' => 556, 'amount' => 7228],
+    ['id' => 'iiiiiiiii7', 'pcs' => 1, 'per_price' => 1000, 'amount' => 1000],
+    ['id' => 'iiiiiiiiii1', 'pcs' => 10, 'per_price' => 1000, 'amount' => 10000],
+    ['id' => 'iiiiiiiiii2', 'pcs' => 9, 'per_price' => 1200, 'amount' => 10800],
+    ['id' => 'iiiiiiiiii3', 'pcs' => 6, 'per_price' => 800, 'amount' => 4800],
+    ['id' => 'iiiiiiiiii4', 'pcs' => 3, 'per_price' => 600, 'amount' => 1800],
+    ['id' => 'iiiiiiiiii5', 'pcs' => 20, 'per_price' => 400, 'amount' => 8000],
+    ['id' => 'iiiiiiiiii6', 'pcs' => 13, 'per_price' => 556, 'amount' => 7228],
+    ['id' => 'iiiiiiiiii7', 'pcs' => 1, 'per_price' => 1000, 'amount' => 1000],
+    ['id' => 'iiiiiiiiiii1', 'pcs' => 10, 'per_price' => 1000, 'amount' => 10000],
+    ['id' => 'iiiiiiiiiii2', 'pcs' => 9, 'per_price' => 1200, 'amount' => 10800],
+    ['id' => 'iiiiiiiiiii3', 'pcs' => 6, 'per_price' => 800, 'amount' => 4800],
+    ['id' => 'iiiiiiiiiii4', 'pcs' => 3, 'per_price' => 600, 'amount' => 1800],
+    ['id' => 'iiiiiiiiiii5', 'pcs' => 20, 'per_price' => 400, 'amount' => 8000],
+    ['id' => 'iiiiiiiiiii6', 'pcs' => 13, 'per_price' => 556, 'amount' => 7228],
+    ['id' => 'iiiiiiiiiii7', 'pcs' => 1, 'per_price' => 1000, 'amount' => 1000],
+    ['id' => 'min', 'pcs' => 10000, 'per_price' => 10, 'amount' => 1000],
+];
+
+/**二维数组某key最大的元素 */
+function array_min($arr, $key){
+    $min_v = null;
+    foreach($arr as $k => $v){
+        if($min_v == null){
+            $min_v = $v;
+            continue;
+        }
+
+        if($min_v[$key] < $v[$key]){
+            $min_v = $v;
+        }
+    }
+
+    return $min_v;
+}
+
+/**找能凑单的明细 */
+function findItem($amount, $items){
+    $key_diff = [];
+    foreach($items as $k => $v){
+        if($amount >= $v['amount']){
+            $key_diff[] = [
+                'k' => $k,
+            ];
+        }
+    }
+
+    if(count($key_diff) == 0){
+        return [
+            'done' => true,
+            'success' => false
+        ];
+    }
+
+    $min_key_diff = $key_diff[0];
+    $item = $items[$min_key_diff['k']];
+    unset($items[$min_key_diff['k']]);
+
+    $balance = $amount - $item['amount'];
+    
+    return [
+        'done' => $balance == 0 ? true : false,
+        'item' => $item,
+        'items' => $items,
+        'balance' => $balance,
+        'success' => true,
+    ];
+
+}
+
+/**最后一点剩余金额用玻璃片凑 */
+function findPcs($amount, $items){
+    $key_diff = [];
+    foreach($items as $k => $v){
+        if($amount >= $v['per_price']){
+            $key_diff[] = [
+                'k' => $k,
+                'balance' => $amount % $v['per_price'],
+            ];
+        }
+    }
+
+    if(count($key_diff) == 0){
+        return ['success' => false];
+    }
+
+    $min_key_diff = array_min($key_diff, 'balance');
+    $min_diff_item = $items[$min_key_diff['k']];
+    $min_diff_pcs = floor($amount / $min_diff_item['per_price']);
+    $balance = $min_key_diff['balance'];
+    $min_diff_item['pcs'] = $min_diff_pcs;
+    $items[$min_key_diff['k']]['pcs'] = $items[$min_key_diff['k']]['pcs'] - $min_diff_pcs;
+
+    return [
+        'item' => $min_diff_item,
+        'items' => $items,
+        'balance' => $balance,
+        'success' => true
+    ];
+
+}
+
+/**玻璃订单用明细凑单 */
+function pieceOrderAmount($orders, $items){
+    $per_prices = array_column($items, 'per_price');
+    array_multisort($per_prices,SORT_DESC ,SORT_NUMERIC,$items);
+
+    $amounts = array_column($orders, 'amount');
+    array_multisort($amounts,SORT_DESC ,SORT_NUMERIC,$orders);
+
+    foreach($orders as $order_k => $order_v ){
+        $order_items_amount = 0;
+        $order_items = [];
+        $balance = $order_v['amount'];
+        $is_breake = false;
+        $item_length = count($items);
+
+        foreach($items as $item_k => $item_v){
+            if($order_v['amount'] >= $order_items_amount + $item_v['amount']){
+
+                if($item_length == $item_k+1 || $order_v['amount'] == $order_items_amount + $item_v['amount']){
+                    $is_breake = true;
+                }
+                $order_items_amount += $item_v['amount'];
+                $order_items[] = $item_v;
+                unset($items[$item_k]);
+                $balance = $order_v['amount'] - $order_items_amount;
+            }
+            else{
+                $items_done = false;
+
+                while(!$items_done){
+                    $items_result = findItem($balance, $items);
+                    $items_done = $items_result['done'];
+                    if($items_result['success']){
+                        $items = $items_result['items'];
+                        $order_items[] = $items_result['item'];
+                        $balance = $items_result['balance'];
+                    }
+                }
+
+                $pcs_result = findPcs($balance, $items);
+                if($pcs_result['success']){
+                    $items = $pcs_result['items'];
+                    $order_items[] = $pcs_result['item'];
+                    $balance = $pcs_result['balance'];
+                }
+
+                
+                $is_breake = true;
+            }
+
+            if($is_breake){
+                $orders[$order_k]['order_items'] = $order_items;
+                $orders[$order_k]['balance'] = $balance;
+                break;
+            }
+           
+        }
+    }
+
+    return $orders;
+}
+
+$orders = pieceOrderAmount($orders, $items);
+
+var_dump(json_encode($orders));
