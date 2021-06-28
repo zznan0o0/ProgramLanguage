97e4729073bfa6a41117a8eab1360b2c120c7c38
kenshin <dx0w0xb@lookout.com>
2021-06-28 18:04:52 +0800

    php push

--- php/array/test.php
+++ php/array/test.php
@@ -2,11 +2,26 @@
 
 $a = '[4,5,6]';
 
-var_dump(json_decode($a, true));
+// var_dump(json_decode($a, true));
 
-// eval('$arr = '.$a.';');
 
-// function stringToArray($str){
-//     var_dump(eval("return " . $str . ";"));
-// }
+function stringToArray($str){
+    return eval("return " . $str . ";");
+}
 // var_dump(stringToArray($a));
+
+
+
+
+$a1 = [1,2,3,4,5,6];
+$a2 = [5,4,3,"2"];
+
+$ia = array_intersect($a1, $a2);
+
+var_dump($ia);
+
+
+$ma = array_merge($a1, $a2);
+$ma = array_values(array_unique($ma));
+// var_dump($ma);
+
